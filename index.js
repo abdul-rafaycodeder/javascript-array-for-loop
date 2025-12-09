f687fef5a001db76e1ae0955c3d882aa257b1513
abdul rafay <abdulrafaycodeder@email.com>
2025-12-09 06:47:44 -0800

    start kavascript class 6

---
+++ index.js
@@ -0,0 +1,41 @@
+// for (let count = 0; count < array.length; count++) {
+//     const element = array[count];
+
+// }
+
+// for (let count = 1; count <= 5; count++) {
+//     console.log("my name is abdul rafay")
+// }
+
+
+
+
+
+
+// for (var a = 1; a <= 5; a++) {
+//     console.log(a)
+// }
+
+// var students = ["ali", "rafay", "osama", "bilal","oo"]
+
+// for (i = 0; i < students.length; i++) {
+//     console.log(students[i])
+// }
+
+//  console.log(students[i])
+
+// var numbers = [1, 2, 3, 4, 5]
+// totle = 0
+
+// for (i = 0; i < numbers.length; i++) {
+
+//     totle = totle + numbers[i]
+//     console.log(totle)
+// }
+
+var arr = ["ali","rafay",10,20]
+
+console.log(arr.length)
+console.log(arr.length-2)
+
+
